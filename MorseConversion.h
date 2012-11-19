3567172897aaa3ad7fc59e5959925691111ba4f4
Jesper Persson <jesperpersson@Jesper-Perssons-MacBook.local>
2012-11-19 23:18:34 +0100

    Updated documentation

--- MorseConversion.h
+++ MorseConversion.h
@@ -15,7 +15,7 @@
 // Given a character, will return the
 // index of that character. Sorted alphabetically.
 //
-// Param - The char which index is to be found.
+// Param - The char whose index is to be found.
 // Return - Int index, the index of the character.
 //
 int index_of_char(char character);
@@ -28,7 +28,14 @@ int index_of_char(char character);
 // Return - char*, the morse code for the character.
 char* latin_to_morse(char character);
 
-
+//
+// Given a char array, will convert each
+// letter to morse-code, and print it.
+// Signs not found in the Latin alphabet
+// will be ignored.
+//
+// Param - The char array to be printed
+// Return - None.
 void print_latin_in_morse(char sentance[] );
 
 #endif
