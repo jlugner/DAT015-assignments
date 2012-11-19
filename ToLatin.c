bfcdb2d78224815c489c46eafb93531dd66665f7
Pungsnigel <JesperT.Persson@gmail.com>
2012-11-20 00:07:35 +0100

    Started implementing main function

---
+++ ToLatin.c
@@ -0,0 +1,18 @@
+//
+//  ToLatin.c
+//  MOP.Assignments
+//
+//  Created by Jesper Persson on 2012-11-19.
+//  Copyright (c) 2012 Jesper Persson. All rights reserved.
+//
+
+#include <stdio.h>
+#include "MorseConversion.h"
+
+void print_morse_in_latin()
+{
+    char input[75];
+    fgets(input, 75, stdin);
+    
+    
+}
