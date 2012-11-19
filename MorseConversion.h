cebbfc10971005a2390c3f54a7f3a03540b14d72
Jesper Persson <jesperpersson@Jesper-Perssons-MacBook.local>
2012-11-20 00:06:39 +0100

    Declared functions for converting morse-code

--- MorseConversion.h
+++ MorseConversion.h
@@ -21,12 +21,32 @@
 int index_of_char(char character);
 
 //
+// Given the morse-code for a letter
+// in the alphabet, the index of
+// that letter will be returned. Should
+// a invalid code be given as input, -1 will
+// be returned.
+//
+// Param - The morse code for the letter
+// Return - -1 if invalid input, else the index of the given letter
+int index_of_morse ( char *word);
+
+//
 // Given a char, will return the morse-code for
 // that character.
 //
 // Param - the character
 // Return - char*, the morse code for the character.
 char* latin_to_morse(char character);
+
+//
+// Given a morse-code representing a letter
+// the char representing that letter
+// will be returned.
+//
+// Param - The morse code for the letter
+// Return - The character
+char morse_to_latin(char *letter);
 
 //
 // Given a char array, will convert each
@@ -37,5 +57,7 @@ char* latin_to_morse(char character);
 // Param - The char array to be printed
 // Return - None.
 void print_latin_in_morse(char sentance[] );
+
+
 
 #endif
